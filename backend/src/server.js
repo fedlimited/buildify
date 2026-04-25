6e51468e9fc4afc4fc953a06aa414ca816a97ae9
ssministry <your-email@example.com>
2026-04-25 22:51:35 +0300

    Add Netlify domain to CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,6 +46,7 @@ app.use(cors({
     'https://buildify-frontend-rnwia68nf-fedlimiteds-projects.vercel.app',
     'https://www.bochi.ke',
     'https://bochi.ke'
+    'https://bochi-buildify.netlify.app'
   ],
 
 
